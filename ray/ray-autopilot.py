38ece544e734b2d32932ece67d23bab079ae08c2
Derek Xu <xzrderek@gmail.com>
2021-11-21 08:38:47 +0000

    Future work - inference and autopilot on Ray.

---
+++ ray/ray-autopilot.py
@@ -0,0 +1,9 @@
+# Ray-autopilot
+def autopilot_on_ray():
+    """
+    Speed up the dataset collection by running simulation and autopilot in Ray.
+    1. Port oatomobile on Carla latest (0.9.12+) version and Python 3.7.
+    2. Solve the problem of Ray supporting ABC.
+    3. Use Ray actor and task to make autopilot simulation parrallel and distributive. 
+    """
+    pass
---
+++ ray/ray-inference.py
@@ -0,0 +1,9 @@
+# Ray-autopilot
+def benchmark_on_ray():
+    """
+    Speed up the inference and benchmarking by running inference and benchmarking on Ray.
+    1. Explore Ray Serve.
+    2. Benchmarking on Ray.
+    3. Explore Ray Dataset and Pytorch Lightening. 
+    """
+    pass
